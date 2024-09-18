835b1f332c953f7f70a36bb259c42c500916785b
renato-marciano <remarcia@microsoft.com>
2024-09-18 17:03:36 +0000

    improved script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 # Enable debugging
-set -x
+# set -x
 
 # Generate a suffix based on the current date and time (mmddyyyyhhmm)
 SUFFIX=$(date +"%m%d%Y%H%M")
@@ -19,12 +19,12 @@ OPENAI_DEPLOYMENT_NAME=${OPENAI_DEPLOYMENT_NAME:-"oai-deployment-$SUFFIX"}
 OPENAI_MODEL_NAME=${OPENAI_MODEL_NAME:-"gpt-4"}
 OPENAI_MODEL_VERSION=${OPENAI_MODEL_VERSION:-"turbo-2024-04-09"}
 
-# Create the resource group
+echo "Creating resource group $RES_GROUP in region $RES_REGION"
 az group create \
     --name $RES_GROUP \
     --location $RES_REGION
 
-# Create Computer Vision resource
+echo "Creating Cognitive Services for Computer Vision"
 az cognitiveservices account create \
     --resource-group $RES_GROUP \
     --name $CV_ACCOUNT_NAME \
@@ -33,7 +33,7 @@ az cognitiveservices account create \
     --sku S1 \
     --yes
 
-# Create Speech Services resource
+echo "Creating Cognitive Services for Speech Services"
 az cognitiveservices account create \
   --name $SS_ACCOUNT_NAME \
   --resource-group $RES_GROUP \
@@ -42,7 +42,7 @@ az cognitiveservices account create \
   --location $RES_REGION \
   --yes
 
-# Create OpenAI resource
+echo "Creating Cognitive Services for OpenAI"
 az cognitiveservices account create \
     --name $OPENAI_ACCOUNT_NAME \
     --resource-group $RES_GROUP \
@@ -51,7 +51,7 @@ az cognitiveservices account create \
     --location $RES_REGION \
     --yes
  
-# Deploy the model for chatcompletion
+echo "Creating OpenAI deployment"
 az cognitiveservices account deployment create \
     --resource-group $RES_GROUP \
     --name $OPENAI_ACCOUNT_NAME \
@@ -60,27 +60,24 @@ az cognitiveservices account deployment create \
     --model-version $OPENAI_MODEL_VERSION \
     --model-format OpenAI
 
-# Get cognitive account endpoint
+echo "Getting keys and endpoint for the resources"
 ACCOUNT_ENDPOINT=$(az cognitiveservices account show \
   --name $CV_ACCOUNT_NAME \
   --resource-group $RES_GROUP \
   --query "properties.endpoint")
 
-# Get key for computer vision resource
 CV_ACCOUNT_KEY=$(az cognitiveservices account keys list \
     --resource-group $RES_GROUP \
     --name $CV_ACCOUNT_NAME \
     --query key1 \
     --output tsv)
 
-# Get the key for speech services resource
 SS_ACCOUNT_KEY=$(az cognitiveservices account keys list \
   --name $SS_ACCOUNT_NAME \
   --resource-group $RES_GROUP \
   --query key1 \
   --output tsv)
 
-# Get the keys and endpoint for OpenAI resource
 OPENAI_ACCOUNT_KEY=$(az cognitiveservices account keys list \
     --resource-group $RES_GROUP \
     --name $OPENAI_ACCOUNT_NAME \
@@ -88,12 +85,14 @@ OPENAI_ACCOUNT_KEY=$(az cognitiveservices account keys list \
     --output tsv)
 
 
-
+echo "Generating local variables file"
 echo "export SS_ACCOUNT_KEY=$SS_ACCOUNT_KEY" >> variables-$SUFFIX.local
 echo "export RES_REGION=$RES_REGION" >> variables-$SUFFIX.local
 echo "export OPENAI_ACCOUNT_KEY=$OPENAI_ACCOUNT_KEY" >> variables-$SUFFIX.local
 echo "export ACCOUNT_ENDPOINT=$ACCOUNT_ENDPOINT" >> variables-$SUFFIX.local
 echo "export OPENAI_VERSION=2023-03-15-preview" >> variables-$SUFFIX.local
+echo "az group delete --name $RES_GROUP --yes --no-wait" >> delete-rg-$SUFFIX.sh
+chmod +x delete-rg-$SUFFIX.sh
 
 echo "export CV_ACCOUNT_KEY=$CV_ACCOUNT_KEY" >> variables-$SUFFIX.local
 echo "export OPENAI_DEPLOYMENT_NAME=$OPENAI_DEPLOYMENT_NAME" >> variables-$SUFFIX.local
@@ -101,5 +100,3 @@ echo "export OPENAI_DEPLOYMENT_NAME=$OPENAI_DEPLOYMENT_NAME" >> variables-$SUFFI
 echo "Variables for this session have been saved to variables-$SUFFIX.local"
 echo "source variables-$SUFFIX.local"
 
-echo "To remove the resources, run:"
-echo "az group delete --name $RES_GROUP --yes"
